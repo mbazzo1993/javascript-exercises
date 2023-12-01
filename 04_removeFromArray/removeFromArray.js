52ce37fddff2eb8d07847d714973573747f02b2c
Matthew Bazzo <matthew.bazzo@slalom.com>
2023-12-01 15:43:37 -0500

    implement removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    if (toRemove.length === 0) return array;
+    
+    let filteredArray = [];
+    for (let i = 0; i < array.length ; i++) {
+        let canIncludeInOutput = true;
+        innerLoop : for (let j = 0; j < toRemove.length; j++) {
+            if (toRemove[j] === array[i]) {
+                canIncludeInOutput = false;
+                break innerLoop;
+            }
+        }
+        if (canIncludeInOutput) filteredArray.push(array[i]);
+    }
 
+    return filteredArray;
 };
 
 // Do not edit below this line

939b40a7b82c2c19bae8aec38aadbc8f965997c0
Matthew Bazzo <matthew.bazzo@slalom.com>
2023-12-01 15:13:40 -0500

    implement reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    for (let i = string.length; i > 0; i--) {
+        output += string.slice(i-1,i);
+    }
+    return output;
 };
 
 // Do not edit below this line

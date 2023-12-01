f639030322bd2bca3c92abae965e0240d44535e8
Matthew Bazzo <matthew.bazzo@slalom.com>
2023-12-01 15:53:24 -0500

    implemented sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (num1 === num2) return num1;
 
+    let max = num1 > num2 ? num1 : num2;
+    let min = num1 === max ? num2 : num1;
+
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

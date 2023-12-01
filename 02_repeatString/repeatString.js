acfb542afb36f890a43641bd439da03d27446ca2
Matthew Bazzo <matthew.bazzo@slalom.com>
2023-12-01 15:00:00 -0500

    completed repeatString challenge and passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-    return 'heyheyhey';
+const repeatString = function(string, repeats) {
+    if (repeats === 0) return "";
+    if (repeats < 0) return 'ERROR';
+    
+    let output = "";
+
+    for (let i = 0; i < repeats; i++) {
+        output += string;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
